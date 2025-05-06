6635133ecf21620760bf51b87c2c3005e1b76153
Pooja <pooja.pimpalshende215@gmail.com>
2025-05-06 15:42:53 +0200

    api call for cabins and added favicon

--- libs/cabins/src/lib/cabins.tsx
+++ libs/cabins/src/lib/cabins.tsx
@@ -1,10 +1,16 @@
 import { Heading, Row } from '@/shared';
+import { useEffect } from 'react';
+import { getCabins } from '@/shared';
 
 export function Cabins() {
+  useEffect(function () {
+    getCabins().then((data) => console.log(data));
+  }, []);
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img src="https://blytefrrzlpznkyszwiv.supabase.co/storage/v1/object/public/cabin-images//cabin-001.jpg" />
     </Row>
   );
 }
