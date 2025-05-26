dbfdbc7c2a6f35f22e2cd108c1425ee7eb5e879c
Pooja <pooja.pimpalshende215@gmail.com>
2025-05-26 16:22:42 +0200

    used react portal for modal to avoid problems like overlay hidden

--- libs/shared/src/lib/components/modal.tsx
+++ libs/shared/src/lib/components/modal.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { HiXMark } from 'react-icons/hi2';
+import { createPortal } from 'react-dom';
 
 const StyledModal = styled.div`
   position: fixed;
@@ -56,7 +57,7 @@ type ModalTypes = {
 };
 
 export const Modal = ({ children, onClose }: ModalTypes) => {
-  return (
+  return createPortal(
     <Overlay>
       <StyledModal>
         <Button onClick={onClose}>
@@ -64,6 +65,7 @@ export const Modal = ({ children, onClose }: ModalTypes) => {
         </Button>
         <div>{children} </div>
       </StyledModal>
-    </Overlay>
+    </Overlay>,
+    document.body
   );
 };
