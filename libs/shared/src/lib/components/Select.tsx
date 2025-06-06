4bee2f6bb61fd9c7d1eed61dd58989db6ba21090
Pooja <pooja.pimpalshende215@gmail.com>
2025-06-06 17:24:17 +0200

    created reusable select component for sort

---
+++ libs/shared/src/lib/components/Select.tsx
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+type SelectProps = {
+  type?: string;
+  value: string;
+};
+
+const StyledSelect = styled.select<SelectProps>`
+  font-size: 1.4rem;
+  padding: 0.8rem 1.2rem;
+  border: 1px solid
+    ${(props) =>
+      props.type === 'white'
+        ? 'var(--color-grey-100)'
+        : 'var(--color-grey-300)'};
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-grey-0);
+  font-weight: 500;
+  box-shadow: var(--shadow-sm);
+`;
+
+export function Select({
+  options,
+  value,
+  ...props
+}: {
+  options: { value: string; label: string }[];
+  value: string;
+  type: string;
+}) {
+  console.log(props);
+  return (
+    <StyledSelect value={value} {...props}>
+      {options.map((option) => (
+        <option value={option.value} key={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </StyledSelect>
+  );
+}
