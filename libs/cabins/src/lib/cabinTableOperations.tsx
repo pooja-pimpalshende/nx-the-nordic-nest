3da241723e88bacd4e83d89bc02d54e31bcb4087
Pooja <pooja.pimpalshende215@gmail.com>
2025-06-08 15:51:41 +0200

    Added sortBy to cabin table operations

--- libs/cabins/src/lib/cabinTableOperations.tsx
+++ libs/cabins/src/lib/cabinTableOperations.tsx
@@ -1,4 +1,4 @@
-import { Filter } from '@/shared';
+import { Filter, SortBy } from '@/shared';
 import { TableOperations } from './tableOperations';
 
 export function CabinTableOperations() {
@@ -10,6 +10,16 @@ export function CabinTableOperations() {
           { value: 'all', label: 'All' },
           { value: 'no-discount', label: 'No discount' },
           { value: 'with-discount', label: 'With discount' },
+        ]}
+      />
+      <SortBy
+        options={[
+          { value: 'name-asc', label: 'Sort by name (A-Z)' },
+          { value: 'name-desc', label: 'Sort by name (Z-A)' },
+          { value: 'regularPrice-asc', label: 'Sort by price (low first)' },
+          { value: 'regularPrice-desc', label: 'Sort by price (high first)' },
+          { value: 'maxCapacity-asc', label: 'Sort by capacity (low first)' },
+          { value: 'maxCapacity-desc', label: 'Sort by capacity (high first)' },
         ]}
       />
     </TableOperations>
