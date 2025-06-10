1f600bbce15c6c6f27f482d03ff7a2f3a71c5ebc
Pooja <pooja.pimpalshende215@gmail.com>
2025-06-10 15:13:30 +0200

    created booking table

---
+++ libs/bookings/src/lib/bookingsTable.tsx
@@ -0,0 +1,28 @@
+import { Booking, Empty, Menus, Table } from '@/shared';
+import { BookingRow } from './bookingRow';
+
+export function BookingsTable() {
+  const bookings: Booking[] = [];
+
+  if (!bookings.length) return <Empty resourceName="bookings" />;
+  return (
+    <Menus>
+      <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
+        <Table.Header>
+          <div>Cabin</div>
+          <div>Guest</div>
+          <div>Dates</div>
+          <div>Status</div>
+          <div>Amount</div>
+        </Table.Header>
+
+        <Table.Body
+          data={bookings}
+          render={(booking) => (
+            <BookingRow key={booking.id} booking={booking} />
+          )}
+        />
+      </Table>
+    </Menus>
+  );
+}
