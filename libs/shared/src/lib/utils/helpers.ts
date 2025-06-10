460579ded8c3bed1929bd0a6a2610c8a82eb9a75
Pooja <pooja.pimpalshende215@gmail.com>
2025-06-10 15:04:55 +0200

    helper function for formatDistanceFromNow

--- libs/shared/src/lib/utils/helpers.ts
+++ libs/shared/src/lib/utils/helpers.ts
@@ -1,4 +1,13 @@
+import { formatDistance, parseISO } from 'date-fns';
+
 export const formatCurrency = (value: number) =>
   new Intl.NumberFormat('en', { style: 'currency', currency: 'USD' }).format(
     value
   );
+
+export const formatDistanceFromNow = (dateStr: string) =>
+  formatDistance(parseISO(dateStr), new Date(), {
+    addSuffix: true,
+  })
+    .replace('about', '')
+    .replace('in', '');
