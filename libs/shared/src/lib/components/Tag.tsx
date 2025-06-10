58af4b6558abe5d3f0956ee3c934476955e66972
Pooja <pooja.pimpalshende215@gmail.com>
2025-06-10 15:04:01 +0200

    reusable tag component

---
+++ libs/shared/src/lib/components/Tag.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+type TagProp = {
+  type: 'blue' | 'green' | 'silver';
+};
+
+export const Tag = styled.span<TagProp>`
+  width: fit-content;
+  text-transform: uppercase;
+  font-size: 1.1rem;
+  font-weight: 600;
+  padding: 0.4rem 1.2rem;
+  border-radius: 100px;
+
+  color: var(--color-${(props) => props.type}-700);
+  background-color: var(--color-${(props) => props.type}-100);
+`;
