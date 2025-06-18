dbfbb9be78d6521f1ec5ca49343a6dd007bb74a1
Pooja <pooja.pimpalshende215@gmail.com>
2025-06-18 16:11:13 +0200

    uploader to upload sample data

---
+++ libs/layout/src/lib/components/uploader.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/shared';
+
+export function Uploader() {
+  return (
+    <div>
+      <h3>SAMPLE DATA</h3>
+      <Button>Upload ALL</Button>
+      <Button>Upload bookings ONLY</Button>
+    </div>
+  );
+}
