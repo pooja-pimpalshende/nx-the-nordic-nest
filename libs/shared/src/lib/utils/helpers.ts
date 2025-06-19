160af0ebd929f9e7985614643dc470f33d200208
Pooja <pooja.pimpalshende215@gmail.com>
2025-06-19 14:59:02 +0200

    added helper function for subtractDates

--- libs/shared/src/lib/utils/helpers.ts
+++ libs/shared/src/lib/utils/helpers.ts
@@ -1,4 +1,7 @@
-import { formatDistance, parseISO } from 'date-fns';
+import { differenceInDays, formatDistance, parseISO } from 'date-fns';
+
+export const subtractDates = (dateStr1: string, dateStr2: string) =>
+  differenceInDays(parseISO(String(dateStr1)), parseISO(String(dateStr2)));
 
 export const formatCurrency = (value: number) =>
   new Intl.NumberFormat('en', { style: 'currency', currency: 'USD' }).format(
