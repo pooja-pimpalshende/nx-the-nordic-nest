0d9ccef5e69bcf891040899dec496175e38ad187
Pooja <pooja.pimpalshende215@gmail.com>
2025-06-19 14:29:24 +0200

    added function for delete and adding cabin,  booking, guest

--- libs/layout/src/lib/components/uploader.tsx
+++ libs/layout/src/lib/components/uploader.tsx
@@ -1,10 +1,61 @@
-import { Button } from '@/shared';
+import { Button, supabase } from '@/shared';
+import { useState } from 'react';
+
+import { guests } from '@/cabins';
+import { cabins } from '@/cabins';
+import { bookings } from '@/bookings';
+
+async function deleteGuests() {
+  const { error } = await supabase.from('guests').delete().gt('id', 0);
+  if (error) console.log(error.message);
+}
+
+async function deleteCabins() {
+  const { error } = await supabase.from('cabins').delete().gt('id', 0);
+  if (error) console.log(error.message);
+}
+
+async function deleteBookings() {
+  const { error } = await supabase.from('bookings').delete().gt('id', 0);
+  if (error) console.log(error.message);
+}
+
+async function createGuests() {
+  const { error } = await supabase.from('guests').insert(guests);
+  if (error) console.log(error.message);
+}
+
+async function createCabins() {
+  const { error } = await supabase.from('cabins').insert(cabins);
+  if (error) console.log(error.message);
+}
+
+async function createBookings() {
+  const { error } = await supabase.from('bookings').insert(bookings);
+  if (error) console.log(error.message);
+}
 
 export function Uploader() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function uploadAll() {
+    setIsLoading(true);
+    //Booking needs to be delated first
+    await deleteBookings();
+    await deleteGuests();
+    await deleteCabins();
+
+    //Bookings needs to be created last
+    await createGuests();
+    await createCabins();
+    await createBookings();
+  }
   return (
     <div>
       <h3>SAMPLE DATA</h3>
-      <Button>Upload ALL</Button>
+      <Button onClick={uploadAll} disabled={isLoading}>
+        Upload ALL
+      </Button>
       <Button>Upload bookings ONLY</Button>
     </div>
   );
