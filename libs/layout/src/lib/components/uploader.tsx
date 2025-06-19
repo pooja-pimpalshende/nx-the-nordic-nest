f41236756bfca6c4481bdd27aaeda56e97178c17
Pooja <pooja.pimpalshende215@gmail.com>
2025-06-19 15:00:05 +0200

    logic for uploading sample bookings and styling

--- libs/layout/src/lib/components/uploader.tsx
+++ libs/layout/src/lib/components/uploader.tsx
@@ -1,9 +1,10 @@
-import { Button, supabase } from '@/shared';
+import { Button, subtractDates, supabase } from '@/shared';
 import { useState } from 'react';
 
 import { guests } from '@/cabins';
 import { cabins } from '@/cabins';
 import { bookings } from '@/bookings';
+import { isFuture, isPast, isToday } from 'date-fns';
 
 async function deleteGuests() {
   const { error } = await supabase.from('guests').delete().gt('id', 0);
@@ -31,7 +32,65 @@ async function createCabins() {
 }
 
 async function createBookings() {
-  const { error } = await supabase.from('bookings').insert(bookings);
+  const { data: guestIds } = await supabase
+    .from('guests')
+    .select('id')
+    .order('id');
+  const allGuestIds = guestIds?.map((cabin) => cabin.id) ?? [];
+
+  const { data: cabinIds } = await supabase
+    .from('cabins')
+    .select('id')
+    .order('id');
+  const allCabinIds = cabinIds?.map((cabin) => cabin.id) ?? [];
+
+  const finalBookings = bookings.map((booking) => {
+    const cabin = cabins.at(booking.cabinId - 1);
+    const numNights = subtractDates(booking.endDate, booking.startDate);
+
+    let cabinPrice = 0;
+    if (cabin?.regularPrice && cabin.discount) {
+      cabinPrice = numNights * (cabin?.regularPrice - cabin?.discount);
+    }
+
+    const extraPrice = booking.hasBreakfast
+      ? numNights * 15 * booking.numGuest
+      : 0;
+    const totalPrice = cabinPrice + extraPrice;
+
+    let status;
+    if (
+      isPast(new Date(booking.endDate)) &&
+      !isToday(new Date(booking.endDate))
+    )
+      status = 'checked-out';
+    if (
+      isFuture(new Date(booking.startDate)) ||
+      isToday(new Date(booking.startDate))
+    )
+      status = 'unconfirmed';
+    if (
+      (isFuture(new Date(booking.endDate)) ||
+        isToday(new Date(booking.endDate))) &&
+      isPast(new Date(booking.startDate)) &&
+      !isToday(new Date(booking.startDate))
+    )
+      status = 'checked-in';
+
+    return {
+      ...booking,
+      numNights,
+      cabinPrice,
+      extraPrice,
+      totalPrice,
+      guestId: allGuestIds.at(booking.guestId - 1),
+      cabinId: allCabinIds.at(booking.cabinId - 1),
+      status,
+    };
+  });
+  console.log(finalBookings);
+
+  const { error } = await supabase.from('bookings').insert(finalBookings);
   if (error) console.log(error.message);
 }
 
@@ -51,7 +110,18 @@ export function Uploader() {
     await createBookings();
   }
   return (
-    <div>
+    <div
+      style={{
+        marginTop: 'auto',
+        backgroundColor: '#e0e7ff',
+        padding: '8px',
+        borderRadius: '5px',
+        textAlign: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '8px',
+      }}
+    >
       <h3>SAMPLE DATA</h3>
       <Button onClick={uploadAll} disabled={isLoading}>
         Upload ALL
