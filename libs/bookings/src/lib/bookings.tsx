598e4500b7410b082fd03b1c1e92c714599d5ad6
Pooja <pooja.pimpalshende215@gmail.com>
2025-06-21 17:02:32 +0200

    providing bookingstableOperations to bookings

--- libs/bookings/src/lib/bookings.tsx
+++ libs/bookings/src/lib/bookings.tsx
@@ -1,11 +1,13 @@
 import { Heading, Row } from '@/shared';
 import { BookingsTable } from './bookingsTable';
+import { BookingTableOprations } from './bookingTableOperations';
 
 export function Bookings() {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All bookings</Heading>
+        <BookingTableOprations />
       </Row>
       <BookingsTable />
     </>
