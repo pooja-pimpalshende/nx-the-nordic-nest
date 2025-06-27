dda01458373966c83c3c9ca8e80952fb3f5cb246
Pooja <pooja.pimpalshende215@gmail.com>
2025-06-27 14:19:14 +0200

    filtering bookings

--- libs/bookings/src/lib/hooks/useBookings.ts
+++ libs/bookings/src/lib/hooks/useBookings.ts
@@ -1,14 +1,25 @@
 import { useQuery } from '@tanstack/react-query';
 import { getBookings } from '../services';
+import { useSearch } from '@tanstack/react-router';
 
 export function useBookings() {
+  const searchParams = useSearch({ from: '/app-layout/bookings' });
+
+  const filterValue = searchParams.status;
+
+  const filter =
+    !filterValue || filterValue === 'all'
+      ? null
+      : { field: 'status', value: filterValue };
+  // { field: 'totalPrice', value: 5000, method: 'gte' };
+
   const {
     isPending,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ['bookings'],
-    queryFn: getBookings,
+    queryKey: ['bookings', filter],
+    queryFn: () => getBookings({ filter }),
   });
 
   return { isPending, bookings, error };
--- libs/bookings/src/lib/services/apiBookings.ts
+++ libs/bookings/src/lib/services/apiBookings.ts
@@ -1,9 +1,17 @@
 import { Booking, supabase } from '@/shared';
 
-export async function getBookings(): Promise<Booking[]> {
-  const { data, error } = await supabase
+export async function getBookings({
+  filter,
+}: Record<string, any> = {}): Promise<Booking[]> {
+  let query = supabase
     .from('bookings')
     .select('*, cabins(name), guests(fullName, email)');
+
+  if (filter !== null) query = query.eq(filter.field, filter.value);
+  // query = query[filter.method || 'eq'](filter.field, filter.value);
+  console.log('query', query);
+
+  const { data, error } = await query;
 
   if (error) {
     console.log(error);
