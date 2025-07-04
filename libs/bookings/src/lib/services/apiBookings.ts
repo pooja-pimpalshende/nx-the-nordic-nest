97e12aa6337fa3788a60533c3599daf8cf6d7e5c
Pooja <pooja.pimpalshende215@gmail.com>
2025-07-04 10:19:21 +0200

    api for delete booking

--- libs/bookings/src/lib/services/apiBookings.ts
+++ libs/bookings/src/lib/services/apiBookings.ts
@@ -67,3 +67,13 @@ export async function updateBooking(id: number, obj: Partial<Booking>) {
 
   return data;
 }
+
+export async function deleteBooking(id: number) {
+  const { data, error } = await supabase.from('bookings').delete().eq('id', id);
+
+  if (error) {
+    console.error(error);
+    throw new Error('Booking could not be deleted');
+  }
+  return data;
+}
