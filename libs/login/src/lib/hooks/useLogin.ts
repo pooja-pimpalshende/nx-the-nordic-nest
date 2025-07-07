e55e0962fce3462ebd4e6c35b66ad97d11834c52
Pooja <pooja.pimpalshende215@gmail.com>
2025-07-07 08:47:04 +0200

    setting user query data on success

--- libs/login/src/lib/hooks/useLogin.ts
+++ libs/login/src/lib/hooks/useLogin.ts
@@ -1,17 +1,18 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { LoginApi } from '../services/apiAuth';
+import { loginApi } from '../services/apiAuth';
 import { useNavigate } from '@tanstack/react-router';
 import toast from 'react-hot-toast';
 
 export function useLogin() {
-  // const querClient = useQueryClient();
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const { mutate: login, isPending } = useMutation({
     mutationFn: ({ email, password }: { email: string; password: string }) =>
-      LoginApi({ email, password }),
+      loginApi({ email, password }),
     onSuccess: (user) => {
-      // querClient.setQueriesData(['user'], user);
+      console.log('user', user);
+      queryClient.setQueryData(['user'], user.user);
       navigate({ to: '/dashboard' });
     },
     onError: (err) => {
