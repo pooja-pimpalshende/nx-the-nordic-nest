0f24f38e17acddd5bf3a31272ccde3ba8fafdd68
Pooja <pooja.pimpalshende215@gmail.com>
2025-07-08 08:48:48 +0200

    implemented signup form for creating new user

---
+++ libs/users/src/lib/signUpForm.tsx
@@ -0,0 +1,92 @@
+import { Button, Form, FormRow, Input } from '@/shared';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import { useSignUp } from './hooks';
+
+export type NewUserFormProps = {
+  email: string;
+  fullName: string;
+  password: string;
+  passwordConfirm: string;
+};
+
+export function SignUpForm() {
+  const { signUp, isPending } = useSignUp();
+  const { register, formState, getValues, handleSubmit, reset } =
+    useForm<NewUserFormProps>();
+  const { errors } = formState;
+
+  const onSubmit: SubmitHandler<Partial<NewUserFormProps>> = ({
+    fullName,
+    email,
+    password,
+  }) => {
+    if (fullName && email && password)
+      signUp({ fullName, email, password }, { onSettled: () => reset() });
+  };
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <FormRow label="Full Name" error={errors?.fullName?.message}>
+        <Input
+          type="text"
+          id="fullName"
+          disabled={isPending}
+          {...register('fullName', { required: 'This field is required' })}
+        />
+      </FormRow>
+
+      <FormRow label="Email address" error={errors?.email?.message}>
+        <Input
+          type="text"
+          id="email"
+          disabled={isPending}
+          {...register('email', {
+            required: 'This field is required',
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: 'Provide valid email address',
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow
+        label="Password (min 8 characters)"
+        error={errors?.password?.message}
+      >
+        <Input
+          type="password"
+          id="password"
+          disabled={isPending}
+          {...register('password', {
+            required: 'This field is required',
+            minLength: {
+              value: 8,
+              message: 'Password needs a minimum of 8 characters',
+            },
+          })}
+        />
+      </FormRow>
+
+      <FormRow label="Repeat password" error={errors?.passwordConfirm?.message}>
+        <Input
+          type="password"
+          id="passwordConfirm"
+          disabled={isPending}
+          {...register('passwordConfirm', {
+            required: 'This field is required',
+            validate: (value) =>
+              value === getValues().password || 'Password needs to match',
+          })}
+        />
+      </FormRow>
+
+      <FormRow>
+        <Button variations="secondary" type="reset" disabled={isPending}>
+          Cancel
+        </Button>
+        <Button disabled={isPending}>Create new user</Button>
+      </FormRow>
+    </Form>
+  );
+}
