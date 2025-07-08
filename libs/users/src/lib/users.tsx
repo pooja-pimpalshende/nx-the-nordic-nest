72bb6d09a976e1d766e0e8d8fc0abc3c8c3068c5
Pooja <pooja.pimpalshende215@gmail.com>
2025-07-08 08:49:32 +0200

    rendered sign up from in Users

--- libs/users/src/lib/users.tsx
+++ libs/users/src/lib/users.tsx
@@ -1,5 +1,11 @@
 import { Heading } from '@/shared';
+import { SignUpForm } from './signUpForm';
 
 export function Users() {
-  return <Heading as="h1">Create a new user</Heading>;
+  return (
+    <>
+      <Heading as="h1">Create a new user</Heading>
+      <SignUpForm />
+    </>
+  );
 }
