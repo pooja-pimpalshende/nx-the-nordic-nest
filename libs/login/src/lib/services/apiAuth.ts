4e5bc7e5f8c038af3aba1f231d473e93d9b7d16d
Pooja <pooja.pimpalshende215@gmail.com>
2025-07-08 08:46:29 +0200

    api call for sign up and updating current user

--- libs/login/src/lib/services/apiAuth.ts
+++ libs/login/src/lib/services/apiAuth.ts
@@ -1,4 +1,28 @@
-import { supabase } from '@/shared';
+import { supabase, supabaseUrl } from '@/shared';
+
+type UpdateUserParams = { password: string } | { data: { [key: string]: any } };
+
+export async function signUpApi({
+  fullName,
+  email,
+  password,
+}: {
+  fullName: string;
+  email: string;
+  password: string;
+}) {
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: { data: { fullName, avatar: '' } },
+  });
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return data;
+}
 
 export async function loginApi({
   email,
@@ -37,5 +61,44 @@ export async function logoutApi() {
   const { error } = await supabase.auth.signOut();
   if (error) {
     throw new Error(error.message);
+  }
+}
+
+export async function updateCurrentUser({
+  password,
+  fullName,
+  avatar,
+}: {
+  password?: string;
+  fullName?: string;
+  avatar?: File | null;
+}) {
+  //1.Update the fullname OR password
+  let updatData;
+  if (password) updatData = { password };
+  if (fullName) updatData = { data: { fullName } };
+  if (updatData) {
+    const { data, error } = await supabase.auth.updateUser(updatData);
+    if (error) throw new Error(error.message);
+    if (!avatar) return data;
+
+    //2.Upload avatar image
+    const fileName = `avatar-${data?.user.id}-${Math.random()}`;
+    const { error: storageError } = await supabase.storage
+      .from('avatars')
+      .upload(fileName, avatar);
+
+    if (storageError) throw new Error(storageError.message);
+
+    //3.Update the avatar in the user
+    const { data: updateUserData, error: updateUserError } =
+      await supabase.auth.updateUser({
+        data: {
+          avatar: `${supabaseUrl}/storage/v1/object/public/avatars//${fileName}`,
+        },
+      });
+    if (updateUserError) throw new Error(updateUserError.message);
+
+    return updateUserData;
   }
 }
