fc0c2136cc4c02f69fa5e8f76e80aee2e921c3d8
Pooja <pooja.pimpalshende215@gmail.com>
2025-07-10 12:22:49 +0200

    helper function to get today

--- libs/shared/src/lib/utils/helpers.ts
+++ libs/shared/src/lib/utils/helpers.ts
@@ -3,6 +3,14 @@ import { differenceInDays, formatDistance, parseISO } from 'date-fns';
 export const subtractDates = (dateStr1: string, dateStr2: string) =>
   differenceInDays(parseISO(String(dateStr1)), parseISO(String(dateStr2)));
 
+export const getToday = function (options: { end?: boolean } = {}) {
+  const today = new Date();
+
+  if (options?.end) today.setUTCHours(23, 59, 59, 999);
+  else today.setUTCHours(0, 0, 0, 0);
+  return today.toISOString();
+};
+
 export const formatCurrency = (value: number) =>
   new Intl.NumberFormat('en', { style: 'currency', currency: 'USD' }).format(
     value
