7d6850c162df34bb2e46249013a25f6461010813
Pooja <pooja.pimpalshende215@gmail.com>
2025-07-10 12:18:21 +0200

    added filter in dashboard

---
+++ libs/dashboard/src/lib/dashboardFilter.tsx
@@ -0,0 +1,14 @@
+import { Filter } from '@/shared';
+
+export function DashboardFilter() {
+  return (
+    <Filter
+      filterField="last"
+      options={[
+        { value: '7', label: 'Last 7 days' },
+        { value: '30', label: 'Last 30 days' },
+        { value: '90', label: 'Last 90 days' },
+      ]}
+    />
+  );
+}
